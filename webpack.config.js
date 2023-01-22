96c63005122f0d40d98e78a7a13982bb7e09ac1c
Vafin Artem <a.vafin85@gmail.com>
2023-01-23 00:55:20 +0500

    Добавляет алиасы

--- webpack.config.js
+++ webpack.config.js
@@ -96,6 +96,15 @@ module.exports = {
 	},
 	resolve: {
 		extensions: ['.tsx', '.ts', '.js'],
+		alias: {
+			"@app": path.resolve(__dirname, "src/app"),
+			"@entities": path.resolve(__dirname, "src/entities"),
+			"@features": path.resolve(__dirname, "src/features"),
+			"@pages": path.resolve(__dirname, "src/pages"),
+			"@processes": path.resolve(__dirname, "src/processes"),
+			"@shared": path.resolve(__dirname, "src/shared"),
+			"@widgets": path.resolve(__dirname, "src/widgets"),
+		}
 	},
 	output: {
 		filename: 'bundle.js',
